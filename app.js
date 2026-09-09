e5309b70f74f0918907cd977177ce8c13a92f9ab
SUMAN DAS <sumanda0708@gmail.com>
2026-09-10 00:09:16 +0530

    implementation of spendMOney function, which holds the expense amount. with negative and 0 digits validation. finally pushed into transactions array in the form of object.

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ function createWallet() {
     transactions.push({ id: transactionID++, type: "Deposit", amount: amount, date: day() });
     // console.log(transactions);
   }
+
+  function spendMoney(amount) {
+    if (amount < 0 || amount === 0) {
+      console.log("Invalid Expense Amount");
+      return;
+    }
+    else if (amount > balance) {
+      console.warn("Not enough balance to proceed.");
+      return;
+    }
+
+    balance -= amount;
+    transactions.push({ id: transactionID++, type: "Expense", amount: amount, date: day() });
+    // console.log(transactions);
+  }
 }
