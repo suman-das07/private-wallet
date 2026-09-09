4012706a2c4d7891fa658f385983c2c0149f4d9c
SUMAN DAS <sumanda0708@gmail.com>
2026-09-10 00:07:58 +0530

    implementation of addMoney function, which helps to deposit the amount. with negative and 0 digits validation. finally pushed into transactions array in the form of object.

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ function createWallet() {
   let transactionID = 1;
 
   let transactions = [];
+
+  function addMoney(amount) {
+    if (amount < 0 || amount === 0) {
+      console.log("Invalid Deposit Amount");
+      return;
+    }
+    // transactionID++;
+    balance += amount;
+    transactions.push({ id: transactionID++, type: "Deposit", amount: amount, date: day() });
+    // console.log(transactions);
+  }
 }
